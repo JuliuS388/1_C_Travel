2bae3f9d34edd43a9867a86a11153b8ed95d7dbf
bintang-anindya <220711899@students.uajy.ac.id>
2024-09-12 09:33:34 +0700

    Mengubah Teks Nama
    Mengubah teks nama menjadi Bintang Anindya - 220711899

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('JulJiGilDoTang'),
+          child: Text('Bintang Anindya-220711899'),
         ),
       ),
     );
