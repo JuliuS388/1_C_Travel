b29b17a91a8fb302ac7a5c144fbe7052e338910d
bintang-anindya <220711899@students.uajy.ac.id>
2024-10-03 10:24:53 +0700

    Styling Profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -4,15 +4,22 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('Profile'),
+        title: Text(
+          'Profile',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blueAccent,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () {},
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.settings),
+            icon: Icon(Icons.settings, color: Colors.white),
             onPressed: () {},
           ),
         ],
@@ -21,50 +28,59 @@ class ProfileScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage('assets/profile_image.jpg'),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Panji Anugrah Agung',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            SizedBox(height: 50),
+            Center(
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage('assets/profile_image.jpg'),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Panji Anugrah Agung',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  Text(
+                    'panjianugrah843',
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
               ),
             ),
-            Text(
-              'panjianugrah843',
-              style: TextStyle(
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(12.0),
+            SizedBox(height: 50),
+
+            Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
               ),
               child: ListTile(
-                leading: Icon(Icons.punch_clock_rounded),
-                title: Text('Riwayat Pesanan'),
-                trailing: Icon(Icons.arrow_forward_ios),
+                leading: Icon(Icons.bookmark_border, color: Colors.blueAccent),
+                title: Text('Riwayat Pesanan', style: TextStyle(fontSize: 18)),
+                trailing: Icon(Icons.arrow_forward_ios, color: Colors.blueAccent),
                 onTap: () {
+                  
                 },
               ),
             ),
-            SizedBox(height: 10),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(12.0),
+            SizedBox(height: 15),
+            Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
               ),
               child: ListTile(
-                leading: Icon(Icons.favorite_border),
-                title: Text('Favorit'),
-                trailing: Icon(Icons.arrow_forward_ios),
+                leading: Icon(Icons.favorite_border, color: Colors.redAccent),
+                title: Text('Favorit', style: TextStyle(fontSize: 18)),
+                trailing: Icon(Icons.arrow_forward_ios, color: Colors.redAccent),
                 onTap: () {
-
                 },
               ),
             ),
